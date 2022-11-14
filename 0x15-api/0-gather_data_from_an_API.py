2b3c87241f12df5e4b97a7de1fe7838eab696d13
TM-Simeon <mnaan.simeon@st.futminna.edu.ng>
2022-11-14 15:26:32 +0100

    update

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,8 +5,8 @@ import sys
 if __name__ == '__main__':
     """only run code when ran as main"""
     userId = sys.argv[1]
-    response = requests.get('https://jsonplaceholder.typicode.com/users/{}'"
-    ".format(userId))
+    response = requests.get('https://jsonplaceholder.typicode.com/users/{}'\
+            .format(userId))
     name = response.json().get('name')
     todos = requests.get('https://jsonplaceholder.typicode.com/todos')
     notCompleted = 0
@@ -17,9 +17,9 @@ if __name__ == '__main__':
                 completed += 1
             else:
                 notCompleted += 1
-    print('Employee {} is done with tasks({}/{}):\n'.format(name,"
-    "completed,(completed + notCompleted)))
+    print('Employee {} is done with tasks({}/{}):\n'.format(name,\
+            completed,(completed + notCompleted)))
     for todo in todos.json():
-        if todo.json().get('userId') == int(userid) and "
-        "todo.json().get('completed'):
+        if todo.json().get('userId') == int(userid) and \
+                todo.json().get('completed'):
             print('\t\s{}'.format(todos.json().get('title')))
